0f99eae7db85ba604617e4893649d1138c31e6f7
DaisukeHORII <0515hdsk@gmail.com>
2023-03-25 13:52:37 +0900

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,109 @@
+import { useState, useEffect } from "react";
+import { NextPage } from "next";
+import styled from "@emotion/styled";
+
+type CatImage = {
+  id: string;
+  url: string;
+};
+
+const IndexPage: NextPage = () => {
+  const [catImage, setCatImage] = useState<CatImage | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchCatImage = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch("https://api.thecatapi.com/v1/images/search");
+        const images = await res.json();
+        console.log(images);
+        setCatImage(images[0]);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+      }
+    };
+    fetchCatImage();
+  }, []);
+
+  const handleButtonClick = async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetch("https://api.thecatapi.com/v1/images/search");
+      const images = await res.json();
+      console.log(images);
+      setCatImage(images[0]);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <PageContainer>
+      <LoadingText isLoading={isLoading}>にゃうローディング</LoadingText>
+      <ImageContainer>
+        {catImage && (
+          <CatImageElement
+            src={catImage.url}
+            alt="cat"
+          />
+        )}
+      </ImageContainer>
+      <ButtonContainer>
+        <Button onClick={handleButtonClick}>別の画像を見る</Button>
+      </ButtonContainer>
+    </PageContainer>
+  );
+};
+
+export default IndexPage;
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #f8f8f8;
+  height: 100vh;
+`;
+
+const ButtonContainer = styled.div`
+  position: absolute;
+  bottom: 2rem;
+`;
+
+const Button = styled.button`
+  background-color: #ffcb6b;
+  color: #333;
+  border: none;
+  padding: 0.8rem 1.6rem;
+  border-radius: 0.4rem;
+  font-size: 1.2rem;
+  cursor: pointer;
+`;
+
+const ImageContainer = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CatImageElement = styled.img`
+  max-width: 50%;
+`;
+
+const LoadingText = styled.p<{ isLoading: boolean }>`
+  visibility: ${(props) => (props.isLoading ? "visible" : "hidden")};
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 1.6rem;
+  color: #555;
+  font-weight: bold;
+`;
